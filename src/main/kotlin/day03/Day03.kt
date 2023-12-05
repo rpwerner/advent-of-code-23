117941702b0fc2024926431dcdf4f30051c21499
rpwerner <7578853+rpwerner@users.noreply.github.com>
2023-12-05 20:04:13 +0100

    Delete target from repository and enhance Day 03 logic

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -3,32 +3,10 @@ package day03
 import isNumeric
 import println
 import readInput
-import kotlin.math.sign
+
+val MATCH_SYMBOL_REGEX = "[^0-9.]".toRegex()
 
 fun main() {
-
-    fun hasSymbolHere(x: Int, y: Int, matrixInput: List<List<String>>) : Boolean {
-        return true
-    }
-
-    fun hasAdjacentSymbol(x: Int, y: Int, matrixInput: List<List<String>>): Boolean {
-        println("$x $y")
-        val stringSize = x + y + 1
-
-
-
-        hasSymbolHere(x, y, matrixInput)
-        hasSymbolHere(x, y, matrixInput)
-        hasSymbolHere(x, y, matrixInput)
-        hasSymbolHere(x, y, matrixInput)
-        hasSymbolHere(x, y, matrixInput)
-        hasSymbolHere(x, y, matrixInput)
-        hasSymbolHere(x, y, matrixInput)
-        hasSymbolHere(x, y, matrixInput)
-
-        return true
-    }
-
     fun part1(matrixInput: List<List<String>>): Int {
         var totalSum = 0
         var lastY = 0
@@ -42,7 +20,7 @@ fun main() {
                     lastY = y
                 } else {
                     // not number at all or anymore
-                    if(currentNumberAsStr.isNotEmpty() && hasAdjacentSymbol(x, lastY, matrixInput)) {
+                    if(currentNumberAsStr.isNotEmpty() && hasAdjacentSymbolInRange(x, lastY, matrixInput)) {
                         val currentNumberAsDigit = currentNumberAsStr.toInt()
                         totalSum += currentNumberAsDigit
                         currentNumberAsStr = ""
@@ -69,16 +47,60 @@ fun main() {
     part1(matrixInput).println()
     //part2(input).println()
 }
+fun hasAdjacentSymbolInRange(x: Int, y: Int, matrixInput: List<List<String>>): Boolean {
+
+    for(i in x .. y) {
+        println("Base position $x,$i")
+        val foundSymbol = isSymbolAtPosition(x-1, i-1, matrixInput) || // above left
+                isSymbolAtPosition(x-1, i, matrixInput) || // above
+                isSymbolAtPosition(x-1, i+1, matrixInput) || // above right
+
+                isSymbolAtPosition(x, i+1, matrixInput) || // right
+                isSymbolAtPosition(x, i-1, matrixInput)  || // left
+
+                isSymbolAtPosition(x+1, i-1, matrixInput) ||  // bellow left
+                isSymbolAtPosition(x+1, i, matrixInput) || // bellow
+                isSymbolAtPosition(x+1, i+1, matrixInput) // bellow right
+
+        if(foundSymbol) return foundSymbol
+    }
+
+    return false
+}
+
+fun isSymbolAtPosition(x: Int, y: Int, matrixInput: List<List<String>>) : Boolean {
+    println("Checking position: $x,$y")
+    if(doesPositionExist(x,y,matrixInput.size)) {
+        return MATCH_SYMBOL_REGEX.matches(matrixInput[x][y])
+    }
+
+    return false
+}
+
+fun doesPositionExist(x: Int, y: Int, matrixSize: Int) : Boolean {
+    if(x < 0 || y < 0) {
+        return false
+    } else if(x > matrixSize || y > matrixSize) {
+        return false
+    }
+
+    return true
+}
 
 fun transformToMatrix(input : List<String>) : List<List<String>> {
 
     var inputMatrix = mutableListOf<List<String>>()
-
+    var i = 0
     input.forEach {line ->
         var lineList = mutableListOf<String>()
+        var j = 0
         line.forEach {
+            print("| $i,$j |")
             lineList.add(it.toString())
+            j++
         }
+        println()
+        i++
         inputMatrix.add(lineList)
     }
 
